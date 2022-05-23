58f347bc9b3a5b915bd944699a22bdfcb782c0ca
unknown <celesbmontero@hotmail.com>
2022-05-23 19:20:23 -0300

    problema github.io

--- src/router.ts
+++ src/router.ts
@@ -27,28 +27,32 @@ const routes = [
 
 ];
 
-const BASE_PATH = "/desafio-m5";
+//const BASE_PATH = "/desafio-m5";
 
-function isGithubPages() {
-    return location.host.includes("github.io");
-  }
+//function isGithubPages() {
+//    return location.host.includes("github.io");
+//  }
 
 
 export function initRouter(container: Element){
   
     function goTo(path){
-        const completePath = isGithubPages() ? BASE_PATH + path : path;
-        history.pushState({}, "", completePath);
-     handleRoute(completePath);
+    //    const completePath = isGithubPages() ? BASE_PATH + path : path;
+    //    history.pushState({}, "", completePath);
+    // handleRoute(completePath);
+    history.pushState({}, "", path);
+    handleRoute(path);
     }
 
     function handleRoute(route){
 
-        const newRoute = isGithubPages() ? route.replace(BASE_PATH, "") : route;
+        //const newRoute = isGithubPages() ? route.replace(BASE_PATH, "") : route;
     
         for(const r of routes){
-            if(r.path.test(newRoute)){
-                const el = r.component({goTo : goTo});
+            //if(r.path.test(newRoute)){
+              if(r.path.test(route)) {
+              
+              const el = r.component({goTo : goTo});
                
                 if(container.firstChild){
                     container.firstChild.remove();
